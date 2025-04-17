b325fce817d8424957e9a1ba2f9293a39ee718f5
unknown <ahmed.moh3011@gmail.com>
2025-04-17 21:57:48 +0200

    delete frontend

--- Front-End/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes'; // ✅ Import your routes
-
-bootstrapApplication(AppComponent, appConfig) // Bootstrap the app with the config
-  .catch((err) => console.error(err));
