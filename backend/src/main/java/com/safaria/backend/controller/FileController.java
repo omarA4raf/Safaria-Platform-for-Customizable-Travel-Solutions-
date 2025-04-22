95df93fe683fb5e428f8ccf4c399c9878fab6127
ayman988 <aymanemad222@gmail.com>
2025-04-22 22:43:00 +0200

    fix_iframe_clicking

--- backend/src/main/java/com/safaria/backend/controller/FileController.java
+++ backend/src/main/java/com/safaria/backend/controller/FileController.java
@@ -14,7 +14,7 @@ import java.nio.file.Paths;
 import java.io.IOException;
 import java.util.*;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/")
 public class FileController {
